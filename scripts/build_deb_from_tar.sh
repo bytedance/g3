b5c6be26e32c3c60d4b964ba1fbbc139464d468a
Zhang Jingqiang <zh_jq@outlook.com>
2023-05-03 23:23:08 +0800

    build src deb package when build from tarball

--- scripts/build_deb_from_tar.sh
+++ scripts/build_deb_from_tar.sh
@@ -12,6 +12,19 @@ CODENAME=$(lsb_release -c -s)
 echo "Finalize debian/changelog"
 dch --maintmaint --release --distribution "${CODENAME}" --force-distribution "Finalize changelog"
 
+SOURCE_NAME=$(dpkg-parsechangelog -SSource)
+SOURCE_VERSION=$(dpkg-parsechangelog -SVersion | awk -F'-' '{print $1}')
+
+echo "Repack ${SOURCE_NAME}_${SOURCE_VERSION}.orig.tar.xz"
+PERMISSION_OPTS="--mode=u=rwX,g=rwX,o=rX"
+REPRODUCIBLE_OPTS="--owner=g3:1000 --group=g3:1000 --sort=name ${PERMISSION_OPTS}"
+PROGRESS_OPTS="--checkpoint=100 --checkpoint-action=dot"
+cd ..
+tar -Jcf "${SOURCE_NAME}_${SOURCE_VERSION}.orig.tar.xz" ${REPRODUCIBLE_OPTS} ${PROGRESS_OPTS} ${SOURCE_NAME}-${SOURCE_VERSION}
+cd -
+echo
+
+echo "Building"
 export RUSTFLAGS="--remap-path-prefix ${HOME}=~"
 
-dpkg-buildpackage -b -uc
+dpkg-buildpackage -uc -us
