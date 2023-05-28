3e84294b0cd15fbb9479c0d7bc43d2757179c912
Zhang Jingqiang <zh_jq@outlook.com>
2023-05-28 21:01:18 +0800

    move cpu affinity code to g3-compat

--- lib/g3-compat/src/sched/mod.rs
+++ lib/g3-compat/src/sched/mod.rs
@@ -14,30 +14,7 @@
  * limitations under the License.
  */
 
-use std::path::{Path, PathBuf};
-
-const STATIC_LIB_NAME: &str = "g3-compat";
-
-fn source_dir(os: &str) -> PathBuf {
-    Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("compat-src")
-        .join(os)
-}
-
 #[cfg(target_os = "linux")]
-fn build_linux() {
-    let source_dir = source_dir("linux");
-    cc::Build::new()
-        .cargo_metadata(true)
-        .define("_GNU_SOURCE", "1")
-        .file(source_dir.join("libc.c"))
-        .compile(STATIC_LIB_NAME);
-}
-
-fn main() {
-    cfg_if::cfg_if! {
-        if #[cfg(target_os = "linux")] {
-            build_linux();
-        }
-    }
-}
+mod linux;
+#[cfg(target_os = "linux")]
+pub use linux::CpuAffinity;
