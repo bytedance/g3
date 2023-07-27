714fee2681666852045d62b506cdb42e5ce36f7b
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2023-07-27 10:16:34 +0800

    update to use webpki-roots 0.25

--- lib/g3-types/src/net/rustls/client.rs
+++ lib/g3-types/src/net/rustls/client.rs
@@ -114,7 +114,7 @@ impl RustlsClientConfigBuilder {
         let mut root_store = RootCertStore::empty();
         if !self.no_default_ca_certs {
             if self.use_builtin_ca_certs {
-                root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(
+                root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(
                     |ta| {
                         OwnedTrustAnchor::from_subject_spki_name_constraints(
                             ta.subject,
