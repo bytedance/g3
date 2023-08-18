7ee04c67b46056a249df25844739008444520838
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2023-08-18 15:36:10 +0800

    g3proxy-ctl: support publish dynamic users

--- g3proxy/utils/ctl/src/user_group.rs
+++ g3proxy/utils/ctl/src/user_group.rs
@@ -14,26 +14,42 @@
  * limitations under the License.
  */
 
-use clap::{Arg, ArgMatches, Command};
+use std::path::PathBuf;
+use std::str::FromStr;
+
+use clap::{value_parser, Arg, ArgMatches, Command, ValueHint};
 
 use g3proxy_proto::proc_capnp::proc_control;
 use g3proxy_proto::user_group_capnp::user_group_control;
 
-use super::common::print_list_text;
+use super::common::{parse_operation_result, print_list_text};
 use super::{CommandError, CommandResult};
 
 pub const COMMAND: &str = "user-group";
 
 const COMMAND_ARG_NAME: &str = "name";
+const COMMAND_ARG_FILE: &str = "file";
 
 const SUBCOMMAND_LIST_STATIC_USER: &str = "list-static-user";
 const SUBCOMMAND_LIST_DYNAMIC_USER: &str = "list-dynamic-user";
+const SUBCOMMAND_PUBLISH_DYNAMIC_USER: &str = "publish-dynamic-user";
 
 pub fn command() -> Command {
     Command::new(COMMAND)
         .arg(Arg::new(COMMAND_ARG_NAME).required(true).num_args(1))
         .subcommand(Command::new(SUBCOMMAND_LIST_STATIC_USER))
         .subcommand(Command::new(SUBCOMMAND_LIST_DYNAMIC_USER))
+        .subcommand(
+            Command::new(SUBCOMMAND_PUBLISH_DYNAMIC_USER)
+                .long_flag("publish")
+                .arg(
+                    Arg::new(COMMAND_ARG_FILE)
+                        .required(true)
+                        .num_args(1)
+                        .value_parser(value_parser!(PathBuf))
+                        .value_hint(ValueHint::FilePath),
+                ),
+        )
 }
 
 pub async fn run(client: &proc_control::Client, args: &ArgMatches) -> CommandResult<()> {
@@ -41,10 +57,11 @@ pub async fn run(client: &proc_control::Client, args: &ArgMatches) -> CommandRes
 
     let user_group = super::proc::get_user_group(client, name).await?;
 
-    if let Some((subcommand, _)) = args.subcommand() {
+    if let Some((subcommand, args)) = args.subcommand() {
         match subcommand {
             SUBCOMMAND_LIST_STATIC_USER => list_static_user(&user_group).await,
             SUBCOMMAND_LIST_DYNAMIC_USER => list_dynamic_user(&user_group).await,
+            SUBCOMMAND_PUBLISH_DYNAMIC_USER => publish_dynamic_user(&user_group, args).await,
             cmd => Err(CommandError::Cli(format!("unsupported subcommand {cmd}"))),
         }
     } else {
@@ -62,4 +79,31 @@ async fn list_dynamic_user(client: &user_group_control::Client) -> CommandResult
     let req = client.list_dynamic_user_request();
     let rsp = req.send().promise.await?;
     print_list_text(rsp.get()?.get_result()?)
+}
+
+async fn publish_dynamic_user(
+    client: &user_group_control::Client,
+    args: &ArgMatches,
+) -> CommandResult<()> {
+    let data = if let Some(file) = args.get_one::<PathBuf>(COMMAND_ARG_FILE) {
+        tokio::fs::read_to_string(file).await.map_err(|e| {
+            CommandError::Cli(format!(
+                "failed to read contents of file {}: {e:?}",
+                file.display()
+            ))
+        })?
+    } else {
+        unreachable!()
+    };
+
+    if let Err(e) = serde_json::Value::from_str(&data) {
+        return Err(CommandError::Cli(format!(
+            "the data to publish is not valid json: {e:?}"
+        )));
+    }
+
+    let mut req = client.publish_dynamic_user_request();
+    req.get().set_contents(&data);
+    let rsp = req.send().promise.await?;
+    parse_operation_result(rsp.get()?.get_result()?)
 }
