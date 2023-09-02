df6381a7a5cdc88e62e4887c79658889dc801787
Zhang Jingqiang <zh_jq@outlook.com>
2023-09-02 23:07:34 +0800

    disable deb source package build by default

--- scripts/build_deb_from_tar.sh
+++ scripts/build_deb_from_tar.sh
@@ -5,6 +5,44 @@ set -e
 SCRIPTS_DIR=$(dirname "$0")
 PROJECT_DIR=$(realpath "${SCRIPTS_DIR}/..")
 
+usage () {
+	cat << EOF
+Usage: $0 [options]
+
+Options:
+  -s Builds the source package
+  -h Show usage message
+
+EOF
+}
+
+create_orig_tar () {
+	SOURCE_NAME=$(dpkg-parsechangelog -SSource)
+	SOURCE_VERSION=$(dpkg-parsechangelog -SVersion | awk -F'-' '{print $1}')
+
+	echo "Repack ${SOURCE_NAME}_${SOURCE_VERSION}.orig.tar.xz"
+	PERMISSION_OPTS="--mode=u=rwX,g=rwX,o=rX"
+	REPRODUCIBLE_OPTS="--owner=g3:1000 --group=g3:1000 --sort=name ${PERMISSION_OPTS}"
+	PROGRESS_OPTS="--checkpoint=100 --checkpoint-action=dot"
+	cd ..
+	tar -Jcf "${SOURCE_NAME}_${SOURCE_VERSION}.orig.tar.xz" ${REPRODUCIBLE_OPTS} ${PROGRESS_OPTS} ${SOURCE_NAME}-${SOURCE_VERSION}
+	cd -
+	echo
+}
+
+while getopts hs f
+do
+	case $f in
+	h)
+		usage;exit 0;;
+	s)
+		BUILD_SOURCE="1";;
+	\?)
+		usage;exit 1;;
+	esac
+done
+shift $((OPTIND - 1))
+
 cd "${PROJECT_DIR}"
 
 CODENAME=$(lsb_release -c -s)
@@ -12,19 +50,16 @@ CODENAME=$(lsb_release -c -s)
 echo "Finalize debian/changelog"
 sed -i s/UNRELEASED/${CODENAME}/ debian/changelog
 
-SOURCE_NAME=$(dpkg-parsechangelog -SSource)
-SOURCE_VERSION=$(dpkg-parsechangelog -SVersion | awk -F'-' '{print $1}')
+BUILD_FLAGS="-uc"
 
-echo "Repack ${SOURCE_NAME}_${SOURCE_VERSION}.orig.tar.xz"
-PERMISSION_OPTS="--mode=u=rwX,g=rwX,o=rX"
-REPRODUCIBLE_OPTS="--owner=g3:1000 --group=g3:1000 --sort=name ${PERMISSION_OPTS}"
-PROGRESS_OPTS="--checkpoint=100 --checkpoint-action=dot"
-cd ..
-tar -Jcf "${SOURCE_NAME}_${SOURCE_VERSION}.orig.tar.xz" ${REPRODUCIBLE_OPTS} ${PROGRESS_OPTS} ${SOURCE_NAME}-${SOURCE_VERSION}
-cd -
-echo
+if [ -n "${BUILD_SOURCE}" ]
+then
+	create_orig_tar
+
+	BUILD_FLAGS="-us ${BUILD_FLAGS}"
+fi
 
 echo "Building"
 export RUSTFLAGS="--remap-path-prefix ${HOME}=~"
 
-dpkg-buildpackage -uc -us
+dpkg-buildpackage ${BUILD_FLAGS}
