43cba40433a1fa2bdcc7945f42a54f6249d0c54d
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2023-10-17 15:30:07 +0800

    g3tiles: use trick rustls server session cache

---
+++ lib/g3-types/src/net/rustls/session.rs
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2023 ByteDance and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use std::num::NonZeroUsize;
+use std::sync::Mutex;
+
+use lru::LruCache;
+use rustls::server::StoresServerSessions;
+
+struct CacheSlot {
+    inner: Mutex<LruCache<Vec<u8>, Vec<u8>, ahash::RandomState>>,
+}
+
+impl CacheSlot {
+    fn new(size: NonZeroUsize) -> Self {
+        CacheSlot {
+            inner: Mutex::new(LruCache::with_hasher(size, ahash::RandomState::new())),
+        }
+    }
+}
+
+pub struct RustlsTrickServerSessionCache {
+    slots: [CacheSlot; 16],
+}
+
+impl RustlsTrickServerSessionCache {
+    pub fn new(each_size: usize) -> Self {
+        let each_size = NonZeroUsize::new(each_size)
+            .unwrap_or_else(|| unsafe { NonZeroUsize::new_unchecked(256) });
+        RustlsTrickServerSessionCache {
+            slots: [
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+                CacheSlot::new(each_size),
+            ],
+        }
+    }
+}
+
+impl StoresServerSessions for RustlsTrickServerSessionCache {
+    fn put(&self, key: Vec<u8>, value: Vec<u8>) -> bool {
+        let Some(c) = key.first() else {
+            return false;
+        };
+        let id = *c & 0x0F;
+
+        let Some(slot) = self.slots.get(id as usize) else {
+            return false;
+        };
+
+        let mut cache = slot.inner.lock().unwrap();
+        cache.put(key, value);
+        true
+    }
+
+    fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
+        let c = key.first()?;
+        let id = *c & 0x0F;
+
+        let slot = self.slots.get(id as usize)?;
+        let mut cache = slot.inner.lock().unwrap();
+        cache.get(key).cloned()
+    }
+
+    fn take(&self, key: &[u8]) -> Option<Vec<u8>> {
+        let c = key.first()?;
+        let id = *c & 0x0F;
+
+        let slot = self.slots.get(id as usize)?;
+        let mut cache = slot.inner.lock().unwrap();
+        cache.pop(key)
+    }
+
+    fn can_cache(&self) -> bool {
+        true
+    }
+}
