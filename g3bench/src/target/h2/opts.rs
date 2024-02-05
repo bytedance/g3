3944e9f97177f12625b6889bab3329e53a26c8e0
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2024-02-05 11:28:10 +0800

    g3bench: really use our h2 config

--- g3bench/src/target/h2/opts.rs
+++ g3bench/src/target/h2/opts.rs
@@ -289,7 +289,8 @@ impl BenchH2Args {
 
         let mut client_builder = h2::client::Builder::new();
         client_builder.max_concurrent_streams(0).enable_push(false);
-        let (h2s, h2s_connection) = h2::client::handshake(stream)
+        let (h2s, h2s_connection) = client_builder
+            .handshake(stream)
             .await
             .map_err(|e| anyhow!("h2 handshake failed: {e:?}"))?;
         tokio::spawn(async move {
