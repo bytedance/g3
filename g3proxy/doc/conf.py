bbd17e938b5a18839395e515804f91a0a889779a
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2024-03-27 16:01:38 +0800

    g3proxy version 1.8.1

--- g3proxy/doc/conf.py
+++ g3proxy/doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Zhang Jingqiang'
 author = 'Zhang Jingqiang'
 
 # The full version, including alpha/beta/rc tags
-release = '1.8.0'
+release = '1.8.1'
 
 
 # -- General configuration ---------------------------------------------------
