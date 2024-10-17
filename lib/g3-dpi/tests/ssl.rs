a9c24a8aee5f726775065e1ccb46e4fea54e024d
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2024-10-17 15:15:30 +0800

    g3-dpi: support fragmented TLS record when detect TLS protocol

--- lib/g3-dpi/tests/ssl.rs
+++ lib/g3-dpi/tests/ssl.rs
@@ -28,3 +28,63 @@ fn port443_demo() {
         .unwrap();
     assert_eq!(protocol, Protocol::TlsModern);
 }
+
+#[test]
+fn fragmented_one() {
+    let mut inspector = ProtocolInspector::default();
+    let config = ProtocolInspectionConfig::default();
+
+    const RECORD_1_BYTES: &[u8] = &[
+        0x16, 0x03, 0x01, 0x00, 0x64, 0x01, 0x00, 0x01, 0x8a, 0x03, 0x03,
+    ];
+
+    let protocol = inspector
+        .check_client_initial_data(&config, 443, RECORD_1_BYTES)
+        .unwrap();
+    assert_eq!(protocol, Protocol::TlsModern);
+}
+
+#[test]
+fn fragmented_two() {
+    let mut inspector = ProtocolInspector::default();
+    let config = ProtocolInspectionConfig::default();
+
+    const RECORD_1_BYTES: &[u8] = &[0x16, 0x03, 0x01, 0x00, 0x04, 0x01, 0x00, 0x01, 0x8a];
+    const RECORD_2_BYTES: &[u8] = &[0x16, 0x03, 0x01, 0x00, 0x04, 0x03, 0x03, 0x76, 0xf4];
+
+    let mut data = Vec::new();
+    data.extend_from_slice(RECORD_1_BYTES);
+    assert!(inspector
+        .check_client_initial_data(&config, 443, &data)
+        .is_err());
+    data.extend_from_slice(RECORD_2_BYTES);
+    let protocol = inspector
+        .check_client_initial_data(&config, 443, &data)
+        .unwrap();
+    assert_eq!(protocol, Protocol::TlsModern);
+}
+
+#[test]
+fn fragmented_three() {
+    let mut inspector = ProtocolInspector::default();
+    let config = ProtocolInspectionConfig::default();
+
+    const RECORD_1_BYTES: &[u8] = &[0x16, 0x03, 0x01, 0x00, 0x02, 0x01, 0x00];
+    const RECORD_2_BYTES: &[u8] = &[0x16, 0x03, 0x01, 0x00, 0x02, 0x01, 0x8a];
+    const RECORD_3_BYTES: &[u8] = &[0x16, 0x03, 0x01, 0x00, 0x02, 0x03, 0x03];
+
+    let mut data = Vec::new();
+    data.extend_from_slice(RECORD_1_BYTES);
+    assert!(inspector
+        .check_client_initial_data(&config, 443, &data)
+        .is_err());
+    data.extend_from_slice(RECORD_2_BYTES);
+    assert!(inspector
+        .check_client_initial_data(&config, 443, &data)
+        .is_err());
+    data.extend_from_slice(RECORD_3_BYTES);
+    let protocol = inspector
+        .check_client_initial_data(&config, 443, &data)
+        .unwrap();
+    assert_eq!(protocol, Protocol::TlsModern);
+}
