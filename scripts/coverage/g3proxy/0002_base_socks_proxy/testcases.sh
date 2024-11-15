e3d0f9a4d30ede778b0f35b85bc21a51f4079c2f
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2024-11-15 12:21:42 +0800

    g3proxy: add user coverage test CI

--- scripts/coverage/g3proxy/0002_base_socks_proxy/testcases.sh
+++ scripts/coverage/g3proxy/0002_base_socks_proxy/testcases.sh
@@ -1,22 +1,38 @@
 #!/bin/sh
 
+
+test_socks5_proxy_http()
+{
+	python3 "${PROJECT_DIR}/g3proxy/ci/python3+curl/test_httpbin.py" -x ${SOCKS5_PROXY} -T http://httpbin.local
+	python3 "${PROJECT_DIR}/g3proxy/ci/python3+requests/test_httpbin.py" -x ${SOCKS5_PROXY} -T http://httpbin.local
+}
+
+
+test_socks5_proxy_dns()
+{
+	python3 "${PROJECT_DIR}/scripts/test/socks5_dns_query.py" -x ${SOCKS5_PROXY} --dns-server 127.0.0.1 g3proxy.local httpbin.local -v
+}
+
+
+test_socks4_proxy_http()
+{
+	python3 "${PROJECT_DIR}/g3proxy/ci/python3+curl/test_httpbin.py" -x ${SOCKS4_PROXY} -T http://httpbin.local
+}
+
+
 SOCKS5_PROXY="socks5h://127.0.0.1:1080"
+test_socks5_proxy_http
+test_socks5_proxy_dns
+
+
 SOCKS4_PROXY="socks4a://127.0.0.1:1080"
-
-python3 "${PROJECT_DIR}/g3proxy/ci/python3+curl/test_httpbin.py" -x ${SOCKS5_PROXY} -T http://httpbin.local
-python3 "${PROJECT_DIR}/g3proxy/ci/python3+curl/test_httpbin.py" -x ${SOCKS4_PROXY} -T http://httpbin.local
-
-python3 "${PROJECT_DIR}/g3proxy/ci/python3+requests/test_httpbin.py" -x ${SOCKS5_PROXY} -T http://httpbin.local
-
-python3 "${PROJECT_DIR}/scripts/test/socks5_dns_query.py" -x ${SOCKS5_PROXY} --dns-server 127.0.0.1 g3proxy.local httpbin.local -v
+test_socks4_proxy_http
 
 
 SOCKS5_PROXY="socks5h://127.0.0.1:1081"
+test_socks5_proxy_http
+test_socks5_proxy_dns
+
+
 SOCKS4_PROXY="socks4a://127.0.0.1:1081"
-
-python3 "${PROJECT_DIR}/g3proxy/ci/python3+curl/test_httpbin.py" -x ${SOCKS5_PROXY} -T http://httpbin.local
-python3 "${PROJECT_DIR}/g3proxy/ci/python3+curl/test_httpbin.py" -x ${SOCKS4_PROXY} -T http://httpbin.local
-
-python3 "${PROJECT_DIR}/g3proxy/ci/python3+requests/test_httpbin.py" -x ${SOCKS5_PROXY} -T http://httpbin.local
-
-python3 "${PROJECT_DIR}/scripts/test/socks5_dns_query.py" -x ${SOCKS5_PROXY} --dns-server 127.0.0.1 g3proxy.local httpbin.local -v
+test_socks4_proxy_http
