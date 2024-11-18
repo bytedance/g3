d8910377edc520f9255f712bf2b897ac7922687c
Zhang Jingqiang <zh_jq@outlook.com>
2024-11-18 23:01:31 +0800

    g3proxy: delete outdated ci test

--- scripts/coverage/g3proxy/0007_chain_socks_proxy/testcases.sh
+++ scripts/coverage/g3proxy/0007_chain_socks_proxy/testcases.sh
@@ -7,19 +7,13 @@ test_http_proxy_http_forward
 #test_http_proxy_ftp_over_http
 
 
-SOCKS5_PROXY="socks5h://127.0.0.1:1080"
-test_socks5_proxy_http
-test_socks5_proxy_dns
+for port in 1080 1081 1082 1083
+do
+	SOCKS5_PROXY="socks5h://127.0.0.1:${port}"
+	test_socks5_proxy_http
+	test_socks5_proxy_dns
 
 
-SOCKS4_PROXY="socks4a://127.0.0.1:1080"
-test_socks4_proxy_http
-
-
-SOCKS5_PROXY="socks5h://127.0.0.1:1081"
-test_socks5_proxy_http
-test_socks5_proxy_dns
-
-
-SOCKS4_PROXY="socks4a://127.0.0.1:1081"
-test_socks4_proxy_http
+	SOCKS4_PROXY="socks4a://127.0.0.1:${port}"
+	test_socks4_proxy_http
+done
