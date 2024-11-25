8a555830ad7ee6727ef60497b9906c96e2c82e7a
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2024-11-25 17:08:17 +0800

    g3proxy: add support for periodic FtpOverHttp task log

--- g3proxy/src/log/task/ftp_over_http.rs
+++ g3proxy/src/log/task/ftp_over_http.rs
@@ -39,6 +39,109 @@ pub(crate) struct TaskLogForFtpOverHttp<'a> {
 }
 
 impl TaskLogForFtpOverHttp<'_> {
+    pub(crate) fn log_created(&self, logger: &Logger) {
+        if let Some(user_ctx) = self.task_notes.user_ctx() {
+            if user_ctx.skip_log() {
+                return;
+            }
+        }
+
+        slog_info!(logger, "";
+            "task_type" => "FtpOverHttp",
+            "task_id" => LtUuid(&self.task_notes.id),
+            "task_event" => "created",
+            "stage" => self.task_notes.stage.brief(),
+            "start_at" => LtDateTime(&self.task_notes.start_at),
+            "user" => self.task_notes.raw_user_name(),
+            "server_addr" => self.task_notes.server_addr(),
+            "client_addr" => self.task_notes.client_addr(),
+            "upstream" => LtUpstreamAddr(self.ftp_notes.upstream()),
+            "method" => LtHttpMethod(&self.ftp_notes.method),
+            "uri" => LtHttpUri::new(&self.ftp_notes.uri, self.ftp_notes.uri_log_max_chars),
+            "user_agent" => self.http_user_agent,
+            "wait_time" => LtDuration(self.task_notes.wait_time),
+        )
+    }
+
+    pub(crate) fn log_connected(&self, logger: &Logger) {
+        if let Some(user_ctx) = self.task_notes.user_ctx() {
+            if user_ctx.skip_log() {
+                return;
+            }
+        }
+
+        slog_info!(logger, "";
+            "task_type" => "FtpOverHttp",
+            "task_id" => LtUuid(&self.task_notes.id),
+            "task_event" => "connected",
+            "stage" => self.task_notes.stage.brief(),
+            "start_at" => LtDateTime(&self.task_notes.start_at),
+            "user" => self.task_notes.raw_user_name(),
+            "server_addr" => self.task_notes.server_addr(),
+            "client_addr" => self.task_notes.client_addr(),
+            "upstream" => LtUpstreamAddr(self.ftp_notes.upstream()),
+            "escaper" => self.ftp_notes.control_tcp_notes.escaper.as_str(),
+            "next_bind_ip" => self.ftp_notes.control_tcp_notes.bind.ip().map(LtIpAddr),
+            "next_expire" => self.ftp_notes.control_tcp_notes.expire.as_ref().map(LtDateTime),
+            "ftp_c_bound_addr" => self.ftp_notes.control_tcp_notes.local,
+            "ftp_c_peer_addr" => self.ftp_notes.control_tcp_notes.next,
+            "ftp_c_connect_tries" => self.ftp_notes.control_tcp_notes.tries,
+            "ftp_c_connect_spend" => LtDuration(self.ftp_notes.control_tcp_notes.duration),
+            "method" => LtHttpMethod(&self.ftp_notes.method),
+            "uri" => LtHttpUri::new(&self.ftp_notes.uri, self.ftp_notes.uri_log_max_chars),
+            "user_agent" => self.http_user_agent,
+            "rsp_status" => self.ftp_notes.rsp_status,
+            "wait_time" => LtDuration(self.task_notes.wait_time),
+            "ready_time" => LtDuration(self.task_notes.ready_time),
+            "ftp_c_rd_bytes" => self.ftp_c_rd_bytes,
+            "ftp_c_wr_bytes" => self.ftp_c_wr_bytes,
+        )
+    }
+
+    pub(crate) fn log_periodic(&self, logger: &Logger) {
+        if let Some(user_ctx) = self.task_notes.user_ctx() {
+            if user_ctx.skip_log() {
+                return;
+            }
+        }
+
+        slog_info!(logger, "";
+            "task_type" => "FtpOverHttp",
+            "task_id" => LtUuid(&self.task_notes.id),
+            "task_event" => "periodic",
+            "stage" => self.task_notes.stage.brief(),
+            "start_at" => LtDateTime(&self.task_notes.start_at),
+            "user" => self.task_notes.raw_user_name(),
+            "server_addr" => self.task_notes.server_addr(),
+            "client_addr" => self.task_notes.client_addr(),
+            "upstream" => LtUpstreamAddr(self.ftp_notes.upstream()),
+            "escaper" => self.ftp_notes.control_tcp_notes.escaper.as_str(),
+            "next_bind_ip" => self.ftp_notes.control_tcp_notes.bind.ip().map(LtIpAddr),
+            "next_expire" => self.ftp_notes.control_tcp_notes.expire.as_ref().map(LtDateTime),
+            "ftp_c_bound_addr" => self.ftp_notes.control_tcp_notes.local,
+            "ftp_c_peer_addr" => self.ftp_notes.control_tcp_notes.next,
+            "ftp_c_connect_tries" => self.ftp_notes.control_tcp_notes.tries,
+            "ftp_c_connect_spend" => LtDuration(self.ftp_notes.control_tcp_notes.duration),
+            "ftp_d_bound_addr" => self.ftp_notes.transfer_tcp_notes.local,
+            "ftp_d_peer_addr" => self.ftp_notes.transfer_tcp_notes.next,
+            "ftp_d_connect_tries" => self.ftp_notes.transfer_tcp_notes.tries,
+            "ftp_d_connect_spend" => LtDuration(self.ftp_notes.transfer_tcp_notes.duration),
+            "method" => LtHttpMethod(&self.ftp_notes.method),
+            "uri" => LtHttpUri::new(&self.ftp_notes.uri, self.ftp_notes.uri_log_max_chars),
+            "user_agent" => self.http_user_agent,
+            "rsp_status" => self.ftp_notes.rsp_status,
+            "wait_time" => LtDuration(self.task_notes.wait_time),
+            "ready_time" => LtDuration(self.task_notes.ready_time),
+            "total_time" => LtDuration(self.total_time),
+            "c_rd_bytes" => self.client_rd_bytes,
+            "c_wr_bytes" => self.client_wr_bytes,
+            "ftp_c_rd_bytes" => self.ftp_c_rd_bytes,
+            "ftp_c_wr_bytes" => self.ftp_c_wr_bytes,
+            "ftp_d_rd_bytes" => self.ftp_d_rd_bytes,
+            "ftp_d_wr_bytes" => self.ftp_d_wr_bytes,
+        )
+    }
+
     pub(crate) fn log(&self, logger: &Logger, e: &ServerTaskError) {
         if let Some(user_ctx) = self.task_notes.user_ctx() {
             if user_ctx.skip_log() {
@@ -49,6 +152,7 @@ impl TaskLogForFtpOverHttp<'_> {
         slog_info!(logger, "{}", e;
             "task_type" => "FtpOverHttp",
             "task_id" => LtUuid(&self.task_notes.id),
+            "task_event" => "finished",
             "stage" => self.task_notes.stage.brief(),
             "start_at" => LtDateTime(&self.task_notes.start_at),
             "user" => self.task_notes.raw_user_name(),
