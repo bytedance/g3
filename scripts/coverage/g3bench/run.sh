08576c2f5b49ac7fc38faab96e6a76be0610e2d6
Zhang Jingqiang <zh_jq@outlook.com>
2024-12-08 20:08:49 +0800

    split CI script to multiple files

---
+++ scripts/coverage/g3bench/run.sh
@@ -0,0 +1,35 @@
+# generate resource files
+"${RUN_DIR}"/mkcert.sh
+
+# start g3proxy
+"${PROJECT_DIR}"/target/debug/g3proxy -c "${RUN_DIR}"/g3proxy.yaml -G ${TEST_NAME} &
+PROXY_PID=$!
+
+# start nginx
+[ -d /tmp/nginx ] || mkdir /tmp/nginx
+/usr/sbin/nginx -c "${PROJECT_DIR}"/scripts/coverage/g3bench/nginx.conf
+
+# run g3bench integration tests
+
+export SSL_CERT_FILE="${RUN_DIR}/rootCA.pem"
+
+g3bench()
+{
+	"${PROJECT_DIR}"/target/debug/g3bench --log-error 1 "$@"
+}
+
+set -x
+
+. ${RUN_DIR}/target_dns.sh
+. ${RUN_DIR}/target_h1.sh
+. ${RUN_DIR}/target_h2.sh
+. ${RUN_DIR}/target_keyless_openssl.sh
+. ${RUN_DIR}/target_openssl.sh
+. ${RUN_DIR}/target_rustls.sh
+
+set +x
+
+"${PROJECT_DIR}"/target/debug/g3proxy-ctl -G ${TEST_NAME} -p $PROXY_PID offline
+
+NGINX_PID=$(cat /tmp/nginx.pid)
+kill -INT $NGINX_PID
