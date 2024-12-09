ac274b10e1f42aa9d0faefc62d0268f5a283e46f
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2024-12-09 15:47:40 +0800

    g3iploc: listen to ctrl-c and shutdown gracefully

--- g3iploc/src/frontend/mod.rs
+++ g3iploc/src/frontend/mod.rs
@@ -14,8 +14,114 @@
  * limitations under the License.
  */
 
+use std::net::IpAddr;
+use std::sync::Arc;
+
+use log::warn;
+use tokio::sync::broadcast;
+
+use g3_geoip_types::{IpLocation, IpLocationBuilder};
+use g3_ip_locate::{Request, Response};
+use g3_types::net::UdpListenConfig;
+
 mod stats;
 pub(crate) use stats::FrontendStats;
 
 mod udp_dgram;
-pub(crate) use udp_dgram::UdpDgramFrontend;
+use udp_dgram::UdpDgramFrontend;
+
+pub(super) struct Frontend {
+    io: UdpDgramFrontend,
+    stats: Arc<FrontendStats>,
+}
+
+impl Frontend {
+    pub(super) fn new(
+        listen_config: &UdpListenConfig,
+        stats: Arc<FrontendStats>,
+    ) -> anyhow::Result<Self> {
+        let io = UdpDgramFrontend::new(listen_config)?;
+        Ok(Frontend { io, stats })
+    }
+
+    pub(super) async fn run(
+        self,
+        mut quit_receiver: broadcast::Receiver<()>,
+    ) -> anyhow::Result<()> {
+        let mut recv_buf = [0u8; 1024];
+
+        loop {
+            tokio::select! {
+                biased;
+
+                r = self.io.recv_req(&mut recv_buf) => {
+                    match r {
+                        Ok((len, addr)) => {
+                            self.stats.add_request_total();
+
+                            let req = match Request::parse_req(&recv_buf[..len]) {
+                                Ok(req) => req,
+                                Err(e) => {
+                                    self.stats.add_request_invalid();
+                                    warn!("invalid request: {e:?}");
+                                    continue;
+                                }
+                            };
+                            let Some(ip) = req.ip() else {
+                                self.stats.add_request_invalid();
+                                continue;
+                            };
+
+                            let Some(location) = self.fetch(ip) else {
+                                continue;
+                            };
+
+                            match Response::encode_new(ip, location, 300) {
+                                Ok(buf) => {
+                                    self.stats.add_response_total();
+                                    if self.io.send_rsp(&buf, addr).await.is_err() {
+                                        self.stats.add_response_fail();
+                                    }
+                                }
+                                Err(e) => {
+                                    warn!("failed to encode response for ip {ip}: {e}");
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            warn!("failed to recv req: {e}");
+                        }
+                    }
+                }
+                _ = quit_receiver.recv() => return Ok(()),
+            }
+        }
+    }
+
+    fn fetch(&self, ip: IpAddr) -> Option<IpLocation> {
+        let mut builder = IpLocationBuilder::default();
+
+        if let Some(db) = g3_geoip_db::store::load_country() {
+            if let Some((net, v)) = db.longest_match(ip) {
+                builder.set_network(net);
+                builder.set_country(v.country);
+                builder.set_continent(v.continent);
+            }
+        }
+
+        if let Some(asn_db) = g3_geoip_db::store::load_asn() {
+            if let Some((net, v)) = asn_db.longest_match(ip) {
+                builder.set_network(net);
+                builder.set_as_number(v.number);
+                if let Some(name) = v.isp_name() {
+                    builder.set_isp_name(name.to_string());
+                }
+                if let Some(domain) = v.isp_domain() {
+                    builder.set_isp_domain(domain.to_string());
+                }
+            }
+        }
+
+        builder.build().ok()
+    }
+}
--- g3iploc/src/lib.rs
+++ g3iploc/src/lib.rs
@@ -16,6 +16,9 @@
 
 use std::sync::Arc;
 
+use log::{debug, warn};
+use tokio::sync::{broadcast, mpsc};
+
 pub mod config;
 
 mod build;
@@ -26,17 +29,50 @@ use opts::ProcArgs;
 mod stat;
 
 mod frontend;
-use frontend::{FrontendStats, UdpDgramFrontend};
+use frontend::{Frontend, FrontendStats};
 
 pub async fn run(proc_args: &ProcArgs) -> anyhow::Result<()> {
     let frontend_stats = Arc::new(FrontendStats::default());
+    let (quit_sender, _) = broadcast::channel(1);
+    let (wait_sender, mut wait_receiver) =
+        mpsc::channel(g3_daemon::runtime::worker::worker_count().min(1));
+
     if let Some(stats_config) = g3_daemon::stat::config::get_global_stat_config() {
         stat::spawn_working_thread(stats_config, frontend_stats.clone())?;
     }
 
-    let udp_listen_addr = proc_args.udp_listen_addr();
+    let workers = g3_daemon::runtime::worker::foreach(|h| {
+        let frontend = Frontend::new(proc_args.listen_config(), frontend_stats.clone())?;
+        let quit_receiver = quit_sender.subscribe();
+        let wait_sender = wait_sender.clone();
+        let id = h.id;
+        h.handle.spawn(async move {
+            let _ = frontend.run(quit_receiver).await;
+            let _ = wait_sender.try_send(Some(id));
+        });
+        Ok::<(), anyhow::Error>(())
+    })?;
+    if workers < 1 {
+        let frontend = Frontend::new(proc_args.listen_config(), frontend_stats.clone())?;
+        let quit_receiver = quit_sender.subscribe();
+        let wait_sender = wait_sender.clone();
+        tokio::spawn(async move {
+            let _ = frontend.run(quit_receiver).await;
+            let _ = wait_sender.try_send(None);
+        });
+    }
 
-    let frontend = UdpDgramFrontend::new(udp_listen_addr, frontend_stats).await?;
-    frontend.into_running().await;
+    if let Err(e) = tokio::signal::ctrl_c().await {
+        warn!("failed to recv Ctrl-C signal: {e}");
+    }
+    debug!("received Ctrl-C signal, start shutdown now");
+    drop(quit_sender);
+
+    while let Some(id) = wait_receiver.recv().await {
+        if let Some(id) = id {
+            debug!("all requests in worker {id} served");
+        }
+    }
+    debug!("all requests served, quit now");
     Ok(())
 }
