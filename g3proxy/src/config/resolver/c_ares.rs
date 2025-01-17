eb7b11292c7e80362eaed12394256d347facab21
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2025-01-17 11:57:43 +0800

    g3-resolver: move c-ares yaml parser in

--- g3proxy/src/config/resolver/c_ares.rs
+++ g3proxy/src/config/resolver/c_ares.rs
@@ -15,10 +15,9 @@
  */
 
 use std::collections::BTreeSet;
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
-use std::str::FromStr;
+use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr};
 
-use anyhow::{anyhow, Context};
+use anyhow::anyhow;
 use yaml_rust::{yaml, Yaml};
 
 use g3_resolver::driver::c_ares::CAresDriverConfig;
@@ -89,64 +88,6 @@ impl CAresResolverConfig {
                 self.name = g3_yaml::value::as_metrics_name(v)?;
                 Ok(())
             }
-            "server" => match v {
-                Yaml::String(addrs) => self.parse_server_str(addrs),
-                Yaml::Array(seq) => self.parse_server_array(seq),
-                _ => Err(anyhow!("invalid yaml value type, expect string / array")),
-            },
-            "each_timeout" => {
-                self.driver.set_each_timeout(g3_yaml::value::as_u32(v)?);
-                Ok(())
-            }
-            "each_tries" => {
-                self.driver.set_each_tries(g3_yaml::value::as_u32(v)?);
-                Ok(())
-            }
-            "max_timeout" => {
-                self.driver.set_max_timeout(g3_yaml::value::as_i32(v)?);
-                Ok(())
-            }
-            "udp_max_quires" => {
-                self.driver.set_udp_max_queries(g3_yaml::value::as_i32(v)?);
-                Ok(())
-            }
-            "round_robin" => {
-                self.driver.set_round_robin(g3_yaml::value::as_bool(v)?);
-                Ok(())
-            }
-            "socket_send_buffer_size" => {
-                self.driver.set_so_send_buf_size(g3_yaml::value::as_u32(v)?);
-                Ok(())
-            }
-            "socket_recv_buffer_size" => {
-                self.driver.set_so_recv_buf_size(g3_yaml::value::as_u32(v)?);
-                Ok(())
-            }
-            "bind_ipv4" => {
-                let ip4 = g3_yaml::value::as_ipv4addr(v)?;
-                self.driver.set_bind_ipv4(ip4);
-                Ok(())
-            }
-            "bind_ipv6" => {
-                let ip6 = g3_yaml::value::as_ipv6addr(v)?;
-                self.driver.set_bind_ipv6(ip6);
-                Ok(())
-            }
-            "negative_min_ttl" | "negative_ttl" | "protective_cache_ttl" => {
-                let ttl = g3_yaml::value::as_u32(v)?;
-                self.driver.set_negative_ttl(ttl);
-                Ok(())
-            }
-            "positive_min_ttl" => {
-                let ttl = g3_yaml::value::as_u32(v)?;
-                self.driver.set_positive_min_ttl(ttl);
-                Ok(())
-            }
-            "positive_max_ttl" | "positive_ttl" | "max_cache_ttl" | "maximum_cache_ttl" => {
-                let ttl = g3_yaml::value::as_u32(v)?;
-                self.driver.set_positive_max_ttl(ttl);
-                Ok(())
-            }
             "graceful_stop_wait" => {
                 self.runtime.graceful_stop_wait = g3_yaml::humanize::as_duration(v)?;
                 Ok(())
@@ -155,53 +96,8 @@ impl CAresResolverConfig {
                 self.runtime.protective_query_timeout = g3_yaml::humanize::as_duration(v)?;
                 Ok(())
             }
-            _ => Err(anyhow!("invalid key {k}")),
+            _ => self.driver.set_by_yaml_kv(k, v),
         }
-    }
-
-    fn parse_server_str(&mut self, addrs: &str) -> anyhow::Result<()> {
-        let addrs = addrs.split_whitespace();
-        for (i, addr) in addrs.enumerate() {
-            self.add_server(addr)
-                .context(format!("#{i} is not a valid server"))?;
-        }
-        Ok(())
-    }
-
-    fn parse_server_array(&mut self, addrs: &[Yaml]) -> anyhow::Result<()> {
-        for (i, addr) in addrs.iter().enumerate() {
-            if let Yaml::String(addr) = addr {
-                self.add_server(addr)
-                    .context(format!("#{i} is not a valid server"))?;
-            } else {
-                return Err(anyhow!("#{i} should be a string value"));
-            }
-        }
-        Ok(())
-    }
-
-    fn parse_socket_addr(addr: &str) -> anyhow::Result<SocketAddr> {
-        if let Ok(sock_addr) = SocketAddr::from_str(addr) {
-            Ok(sock_addr)
-        } else if let Ok(ip) = IpAddr::from_str(addr) {
-            let sock_addr = SocketAddr::new(ip, 53);
-            Ok(sock_addr)
-        } else {
-            Err(anyhow!("invalid SocketAddr / IpAddr string {addr}"))
-        }
-    }
-
-    fn add_server(&mut self, addr: &str) -> anyhow::Result<()> {
-        let sock_addr = CAresResolverConfig::parse_socket_addr(addr)?;
-        let ip = sock_addr.ip();
-        if ip.is_unspecified() {
-            return Err(anyhow!("dns server address should not be unspecified"));
-        }
-        if ip.is_multicast() {
-            return Err(anyhow!("dns server address should not be multicast"));
-        }
-        self.driver.add_server(sock_addr);
-        Ok(())
     }
 
     fn check(&mut self) -> anyhow::Result<()> {
