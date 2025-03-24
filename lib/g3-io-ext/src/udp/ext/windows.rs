b82a198fa03b479be9e82e09b2399b9177120554
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2025-03-24 19:23:41 +0800

    g3-io-ext: support recvmsg on Windows

--- lib/g3-io-ext/src/udp/ext/windows.rs
+++ lib/g3-io-ext/src/udp/ext/windows.rs
@@ -14,17 +14,143 @@
  * limitations under the License.
  */
 
-use std::io;
-use std::io::IoSlice;
-use std::net::SocketAddr;
+use std::cell::{RefCell, UnsafeCell};
+use std::io::{IoSlice, IoSliceMut};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
+use std::os::windows::io::AsRawSocket;
+use std::sync::LazyLock;
 use std::task::{Context, Poll, ready};
+use std::time::Duration;
+use std::{io, ptr};
 
 use tokio::io::Interest;
 use tokio::net::UdpSocket;
+use windows_sys::Win32::Networking::WinSock;
 
 use g3_socket::RawSocket;
+use g3_socket::cmsg::udp::{RecvAncillaryBuffer, RecvAncillaryData};
 
 use super::UdpSocketExt;
+
+thread_local! {
+    static RECV_ANCILLARY_BUFFER: RefCell<RecvAncillaryBuffer> = const { RefCell::new(RecvAncillaryBuffer::new()) };
+}
+
+#[derive(Default)]
+struct RawSocketAddr {
+    buf: [u8; size_of::<WinSock::SOCKADDR_IN6>()],
+}
+
+impl RawSocketAddr {
+    unsafe fn get_ptr_and_size(&mut self) -> (*mut WinSock::SOCKADDR, i32) {
+        unsafe {
+            let p = &*(self.buf.as_ptr() as *mut WinSock::SOCKADDR);
+
+            let size = match p.sa_family {
+                WinSock::AF_INET => size_of::<WinSock::SOCKADDR_IN>(),
+                WinSock::AF_INET6 => size_of::<WinSock::SOCKADDR_IN6>(),
+                _ => self.buf.len(),
+            };
+
+            (self.buf.as_mut_ptr() as _, size as i32)
+        }
+    }
+
+    fn to_std(&self) -> Option<SocketAddr> {
+        let p = unsafe { &*(self.buf.as_ptr() as *mut WinSock::SOCKADDR) };
+
+        match p.sa_family {
+            WinSock::AF_INET => {
+                let v4 = unsafe { &*(self.buf.as_ptr() as *const WinSock::SOCKADDR_IN) };
+                Some(SocketAddr::V4(SocketAddrV4::new(
+                    Ipv4Addr::from(u32::from_be(unsafe { v4.sin_addr.S_un.S_addr })),
+                    u16::from_be(v4.sin_port),
+                )))
+            }
+            WinSock::AF_INET6 => {
+                let v6 = unsafe { &*(self.buf.as_ptr() as *const WinSock::SOCKADDR_IN6) };
+                Some(SocketAddr::V6(SocketAddrV6::new(
+                    Ipv6Addr::from(unsafe { v6.sin6_addr.u.Byte }),
+                    u16::from_be(v6.sin6_port),
+                    u32::from_be(v6.sin6_flowinfo),
+                    unsafe { v6.Anonymous.sin6_scope_id },
+                )))
+            }
+            _ => None,
+        }
+    }
+}
+
+pub struct RecvMsgHdr<'a, const C: usize> {
+    pub iov: [IoSliceMut<'a>; C],
+    pub n_recv: usize,
+    c_addr: UnsafeCell<RawSocketAddr>,
+    dst_ip: Option<IpAddr>,
+    interface_id: Option<u32>,
+}
+
+impl<const C: usize> RecvAncillaryData for RecvMsgHdr<'_, C> {
+    fn set_recv_interface(&mut self, id: u32) {
+        self.interface_id = Some(id);
+    }
+
+    fn set_recv_dst_addr(&mut self, addr: IpAddr) {
+        self.dst_ip = Some(addr);
+    }
+
+    fn set_timestamp(&mut self, _ts: Duration) {}
+}
+
+impl<'a, const C: usize> RecvMsgHdr<'a, C> {
+    pub fn new(iov: [IoSliceMut<'a>; C]) -> Self {
+        RecvMsgHdr {
+            iov,
+            n_recv: 0,
+            c_addr: UnsafeCell::new(RawSocketAddr::default()),
+            dst_ip: None,
+            interface_id: None,
+        }
+    }
+
+    pub fn src_addr(&self) -> Option<SocketAddr> {
+        let c_addr = unsafe { &*self.c_addr.get() };
+        c_addr.to_std()
+    }
+
+    pub fn dst_addr(&self, local_addr: SocketAddr) -> SocketAddr {
+        self.dst_ip
+            .map(|ip| SocketAddr::new(ip, local_addr.port()))
+            .unwrap_or(local_addr)
+    }
+
+    #[inline]
+    pub fn interface_id(&self) -> Option<u32> {
+        self.interface_id
+    }
+
+    /// # Safety
+    ///
+    /// `self` should not be dropped before the returned value
+    pub unsafe fn to_msghdr(&self, control_buf: &mut RecvAncillaryBuffer) -> WinSock::WSAMSG {
+        let control_buf = control_buf.as_bytes();
+        unsafe {
+            let c_addr = &mut *self.c_addr.get();
+            let (name, namelen) = c_addr.get_ptr_and_size();
+
+            WinSock::WSAMSG {
+                name,
+                namelen,
+                lpBuffers: self.iov.as_ptr() as _,
+                dwBufferCount: C as _,
+                Control: WinSock::WSABUF {
+                    len: control_buf.len() as _,
+                    buf: control_buf.as_ptr() as _,
+                },
+                dwFlags: 0,
+            }
+        }
+    }
+}
 
 impl UdpSocketExt for UdpSocket {
     fn poll_sendmsg(
@@ -54,4 +180,100 @@ impl UdpSocketExt for UdpSocket {
 
         self.try_io(Interest::WRITABLE, || socket.sendmsg(iov, target))
     }
+
+    fn poll_recvmsg<const C: usize>(
+        &self,
+        cx: &mut Context<'_>,
+        hdr: &mut RecvMsgHdr<'_, C>,
+    ) -> Poll<io::Result<()>> {
+        let wsa_recvmsg_ptr = WSARECVMSG_PTR.expect("valid function pointer for WSARecvMsg");
+
+        RECV_ANCILLARY_BUFFER.with_borrow_mut(|control_buf| {
+            let mut msghdr = unsafe { hdr.to_msghdr(control_buf) };
+
+            let raw_fd = self.as_raw_socket() as usize;
+            let mut recvmsg = || {
+                let mut len = 0;
+                let r = unsafe {
+                    (wsa_recvmsg_ptr)(
+                        raw_fd,
+                        ptr::from_mut(&mut msghdr),
+                        ptr::from_mut(&mut len),
+                        ptr::null_mut(),
+                        None,
+                    )
+                };
+                if r != 0 {
+                    Err(io::Error::last_os_error())
+                } else {
+                    Ok(len as usize)
+                }
+            };
+
+            loop {
+                ready!(self.poll_recv_ready(cx))?;
+                match self.try_io(Interest::READABLE, &mut recvmsg) {
+                    Ok(nr) => {
+                        hdr.n_recv = nr;
+                        control_buf.parse(msghdr.Control.len as _, hdr)?;
+                        return Poll::Ready(Ok(()));
+                    }
+                    Err(e) => {
+                        if e.kind() == io::ErrorKind::WouldBlock {
+                            continue;
+                        } else {
+                            return Poll::Ready(Err(e));
+                        }
+                    }
+                }
+            }
+        })
+    }
 }
+
+static WSARECVMSG_PTR: LazyLock<WinSock::LPFN_WSARECVMSG> = LazyLock::new(|| {
+    let s = unsafe { WinSock::socket(WinSock::AF_INET as _, WinSock::SOCK_DGRAM as _, 0) };
+    if s == WinSock::INVALID_SOCKET {
+        println!(
+            "ignoring WSARecvMsg function pointer due to socket creation error: {}",
+            io::Error::last_os_error()
+        );
+        return None;
+    }
+
+    // Detect if OS expose WSARecvMsg API based on
+    // https://github.com/Azure/mio-uds-windows/blob/a3c97df82018086add96d8821edb4aa85ec1b42b/src/stdnet/ext.rs#L601
+    let guid = WinSock::WSAID_WSARECVMSG;
+    let mut wsa_recvmsg_ptr = None;
+    let mut len = 0;
+
+    // Safety: Option handles the NULL pointer with a None value
+    let rc = unsafe {
+        WinSock::WSAIoctl(
+            s as _,
+            WinSock::SIO_GET_EXTENSION_FUNCTION_POINTER,
+            &guid as *const _ as *const _,
+            size_of_val(&guid) as u32,
+            &mut wsa_recvmsg_ptr as *mut _ as *mut _,
+            size_of_val(&wsa_recvmsg_ptr) as u32,
+            &mut len,
+            ptr::null_mut(),
+            None,
+        )
+    };
+    if rc == -1 {
+        println!(
+            "ignoring WSARecvMsg function pointer due to ioctl error: {}",
+            io::Error::last_os_error()
+        );
+    } else if len as usize != size_of::<WinSock::LPFN_WSARECVMSG>() {
+        println!("ignoring WSARecvMsg function pointer due to pointer size mismatch");
+        wsa_recvmsg_ptr = None;
+    }
+
+    unsafe {
+        WinSock::closesocket(s);
+    }
+
+    wsa_recvmsg_ptr
+});
