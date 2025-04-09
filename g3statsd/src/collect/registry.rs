82da1aac360057656038ecb9218614e9879edb62
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2025-04-09 09:51:22 +0800

    g3statsd: always reload collector when changes needed

--- g3statsd/src/collect/registry.rs
+++ g3statsd/src/collect/registry.rs
@@ -25,72 +25,110 @@ use g3_types::metrics::NodeName;
 use super::ArcCollector;
 use crate::config::collector::AnyCollectorConfig;
 
-static RUNTIME_COLLECTOR_REGISTRY: Mutex<HashMap<NodeName, ArcCollector, FixedState>> =
-    Mutex::new(HashMap::with_hasher(FixedState::with_seed(0)));
+static RUNTIME_COLLECTOR_REGISTRY: Mutex<CollectorRegistry> = Mutex::new(CollectorRegistry::new());
+
+pub(crate) struct CollectorRegistry {
+    inner: HashMap<NodeName, ArcCollector, FixedState>,
+}
+
+impl CollectorRegistry {
+    const fn new() -> Self {
+        CollectorRegistry {
+            inner: HashMap::with_hasher(FixedState::with_seed(0)),
+        }
+    }
+
+    fn add(&mut self, name: NodeName, collector: ArcCollector) {
+        if let Some(old_collector) = self.inner.insert(name, collector) {
+            old_collector._clean_to_offline();
+        }
+    }
+
+    fn del(&mut self, name: &NodeName) {
+        if let Some(old_collector) = self.inner.remove(name) {
+            old_collector._clean_to_offline();
+        }
+    }
+
+    fn foreach<F>(&self, mut f: F)
+    where
+        F: FnMut(&NodeName, &ArcCollector),
+    {
+        for (name, collector) in self.inner.iter() {
+            f(name, collector);
+        }
+    }
+
+    fn get_names(&self) -> HashSet<NodeName> {
+        self.inner.keys().cloned().collect()
+    }
+
+    fn get_config(&self, name: &NodeName) -> Option<AnyCollectorConfig> {
+        self.inner.get(name).map(|collect| collect._clone_config())
+    }
+
+    pub(super) fn reload(
+        &mut self,
+        name: &NodeName,
+        config: Option<AnyCollectorConfig>,
+    ) -> anyhow::Result<()> {
+        let Some(old_collector) = self.inner.get(name) else {
+            return Err(anyhow!("no collector with name {name} found"));
+        };
+        let old_collector = old_collector.clone();
+        let config = config.unwrap_or_else(|| old_collector._clone_config());
+        let collector = old_collector._reload(config, self)?;
+        self.add(name.clone(), collector);
+        Ok(())
+    }
+
+    pub(super) fn get_or_insert_default(&mut self, name: &NodeName) -> ArcCollector {
+        self.inner
+            .entry(name.clone())
+            .or_insert_with(|| super::discard::DiscardCollector::prepare_default(name))
+            .clone()
+    }
+}
 
 pub(super) fn add(name: NodeName, collector: ArcCollector) {
-    let mut ht = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
-    if let Some(old_collector) = ht.insert(name, collector) {
-        old_collector._clean_to_offline();
-    }
+    let mut r = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
+    r.add(name, collector)
 }
 
 pub(super) fn del(name: &NodeName) {
-    let mut ht = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
-    if let Some(old_collector) = ht.remove(name) {
-        old_collector._clean_to_offline();
-    }
+    let mut r = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
+    r.del(name);
 }
 
-pub(crate) fn foreach<F>(mut f: F)
+pub(crate) fn foreach<F>(f: F)
 where
     F: FnMut(&NodeName, &ArcCollector),
 {
-    let ht = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
-    for (name, collector) in ht.iter() {
-        f(name, collector)
-    }
+    let r = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
+    r.foreach(f);
 }
 
 pub(crate) fn get_names() -> HashSet<NodeName> {
-    let mut names = HashSet::new();
-    let ht = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
-    for name in ht.keys() {
-        names.insert(name.clone());
-    }
-    names
-}
-
-pub(super) fn get_collector(name: &NodeName) -> Option<ArcCollector> {
-    let ht = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
-    ht.get(name).cloned()
+    let r = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
+    r.get_names()
 }
 
 pub(super) fn get_config(name: &NodeName) -> Option<AnyCollectorConfig> {
-    let ht = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
-    ht.get(name).map(|collect| collect._clone_config())
+    let r = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
+    r.get_config(name)
 }
 
 pub(super) fn reload_existed(
     name: &NodeName,
     config: Option<AnyCollectorConfig>,
 ) -> anyhow::Result<()> {
-    let Some(old_collector) = get_collector(name) else {
-        return Err(anyhow!("no collector with name {name} found"));
-    };
-    let config = config.unwrap_or_else(|| old_collector._clone_config());
-
-    // the _reload method is allowed to hold a registry lock
-    // a tokio mutex is needed if we lock this await inside
-    let collector = old_collector._lock_safe_reload(config)?;
-
-    add(name.clone(), collector);
-    Ok(())
+    let mut r = RUNTIME_COLLECTOR_REGISTRY
+        .lock()
+        .map_err(|e| anyhow!("failed to lock collector registry: {e}"))?;
+    r.reload(name, config)
 }
 
 pub(crate) fn get_or_insert_default(name: &NodeName) -> ArcCollector {
-    let mut ht = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
-    ht.entry(name.clone())
-        .or_insert_with(|| super::discard::DiscardCollector::prepare_default(name))
-        .clone()
+    let mut r = RUNTIME_COLLECTOR_REGISTRY.lock().unwrap();
+    r.get_or_insert_default(name)
 }
