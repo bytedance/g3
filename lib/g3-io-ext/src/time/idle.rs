2490d926ecba06b75ef5cc105e810925ec574e63
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2025-04-15 16:52:17 +0800

    rollback to use tokio interval timer

--- lib/g3-io-ext/src/time/idle.rs
+++ lib/g3-io-ext/src/time/idle.rs
@@ -15,165 +15,38 @@
  */
 
 use std::sync::Arc;
-use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::Duration;
 
-use tokio::sync::broadcast;
-
-const CHANNEL_SIZE: usize = 8;
-
-struct IdleWheelSlot {
-    senders: [broadcast::Sender<()>; 16],
-}
-
-impl IdleWheelSlot {
-    fn new() -> Self {
-        IdleWheelSlot {
-            senders: [
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-                broadcast::channel(CHANNEL_SIZE).0,
-            ],
-        }
-    }
-
-    fn subscribe(&self) -> broadcast::Receiver<()> {
-        let mut id = 0;
-        let mut min_count = self.senders[0].receiver_count();
-        for i in 1..self.senders.len() {
-            let count = self.senders[i].receiver_count();
-            if min_count > count {
-                id = i;
-                min_count = count;
-            }
-        }
-        // the selection of min sender is not atomic operation,
-        // but we don't care as it doesn't need to be precise
-        self.senders[id].subscribe()
-    }
-
-    fn has_receiver(&self) -> bool {
-        self.senders
-            .iter()
-            .any(|sender| sender.receiver_count() > 0)
-    }
-
-    fn wake(&self) {
-        self.senders.iter().for_each(|sender| {
-            let _ = sender.send(());
-        });
-    }
-}
+use tokio::time::{Instant, Interval};
 
 pub struct IdleWheel {
     interval: Duration,
-    slots: Vec<IdleWheelSlot>,
-    register_index: AtomicUsize,
 }
 
 impl IdleWheel {
     pub fn spawn(interval: Duration) -> Arc<IdleWheel> {
-        let mut interval_seconds = interval.as_secs() as usize;
-        if interval.subsec_nanos() > 0 {
-            interval_seconds += 1;
-        }
-        let slot_count = interval_seconds.max(2);
-        let mut slots = Vec::with_capacity(slot_count);
-        for _ in 0..slot_count {
-            slots.push(IdleWheelSlot::new());
-        }
-
-        let register_index = AtomicUsize::new(slot_count - 1);
-        let mut emit_index = 0;
-
-        let wheel = Arc::new(IdleWheel {
-            interval,
-            slots,
-            register_index,
-        });
-        let wheel_run = wheel.clone();
-
-        tokio::spawn(async move {
-            let mut interval = tokio::time::interval(Duration::from_secs(1));
-
-            loop {
-                interval.tick().await;
-                if Arc::strong_count(&wheel_run) <= 1 {
-                    let has_receiver = wheel_run.slots.iter().any(|slot| slot.has_receiver());
-                    if !has_receiver {
-                        break;
-                    }
-                }
-
-                wheel_run.slots[emit_index].wake();
-                // register new receivers to the last emit slot
-                wheel_run
-                    .register_index
-                    .store(emit_index, Ordering::Release);
-                emit_index += 1;
-                if emit_index >= slot_count {
-                    emit_index = 0;
-                }
-            }
-        });
-
-        wheel
+        Arc::new(IdleWheel { interval })
     }
 
     pub fn register(&self) -> IdleInterval {
-        let id = self.register_index.load(Ordering::Acquire);
-        let receiver = self.slots[id].subscribe();
         IdleInterval {
-            interval: self.interval,
-            receiver,
+            interval: tokio::time::interval_at(Instant::now() + self.interval, self.interval),
         }
     }
 }
 
 pub struct IdleInterval {
-    interval: Duration,
-    receiver: broadcast::Receiver<()>,
+    interval: Interval,
 }
 
 impl IdleInterval {
     pub async fn tick(&mut self) -> usize {
-        use broadcast::error::{RecvError, TryRecvError};
-
-        match self.receiver.recv().await {
-            Ok(_) => 1,
-            Err(RecvError::Closed) => {
-                // the sender won't be dropped if there are receivers
-                unreachable!()
-            }
-            Err(RecvError::Lagged(mut n)) => {
-                loop {
-                    match self.receiver.try_recv() {
-                        Ok(_) => n += 1,
-                        Err(TryRecvError::Empty) => break,
-                        Err(TryRecvError::Lagged(m)) => n += m,
-                        Err(TryRecvError::Closed) => unreachable!(),
-                    }
-                }
-                usize::try_from(n).unwrap_or(CHANNEL_SIZE)
-            }
-        }
+        self.interval.tick().await;
+        1
     }
 
     pub fn period(&self) -> Duration {
-        self.interval
+        self.interval.period()
     }
 }
 
