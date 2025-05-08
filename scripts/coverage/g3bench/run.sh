dcd53246abcc1846c0e6085816cef46822af9365
Zhang Jingqiang <zh_jq@outlook.com>
2025-05-08 23:12:24 +0800

    enable graphite as statsd backend in CI

--- scripts/coverage/g3bench/run.sh
+++ scripts/coverage/g3bench/run.sh
@@ -9,6 +9,12 @@ PROXY_PID=$!
 [ -d /tmp/nginx ] || mkdir /tmp/nginx
 /usr/sbin/nginx -c "${PROJECT_DIR}"/scripts/coverage/g3bench/nginx.conf
 
+# start g3statsd
+[ -n "${INFLUX_TOKEN}" ] || INFLUX_TOKEN=$(curl -X POST http://127.0.0.1:8181/api/v3/configure/token/admin | jq ".token" -r)
+export INFLUX_TOKEN
+"${PROJECT_DIR}"/target/debug/g3statsd -c "${RUN_DIR}"/g3statsd.yaml -G ${TEST_NAME} &
+STATSD_PID=$!
+
 # run g3bench integration tests
 
 export TEST_CA_CERT_FILE="${RUN_DIR}/rootCA.pem"
@@ -34,5 +40,6 @@ set +x
 
 "${PROJECT_DIR}"/target/debug/g3proxy-ctl -G "${TEST_NAME}" -p $PROXY_PID offline
 
+kill -INT $STATSD_PID
 NGINX_PID=$(cat /tmp/nginx.pid)
 kill -INT $NGINX_PID
