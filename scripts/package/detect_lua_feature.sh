9464beee8a54c3409e8369cdc092e118c1fdc701
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2025-07-15 09:34:52 +0800

    drop support for lua 5.1

--- scripts/package/detect_lua_feature.sh
+++ scripts/package/detect_lua_feature.sh
@@ -6,7 +6,7 @@ if $(pkg-config --exists lua)
 then
 	pkg-config --variable=lib_name lua | tr -d '.'
 else
-	for lib in lua54 lua53 lua51
+	for lib in lua54 lua53
 	do
 		if $(pkg-config --exists ${lib})
 		then
