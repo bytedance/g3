e2a1e0aeb33f286e3b243ad86d335de668d9eec6
Zhang Jingqiang <zh_jq@outlook.com>
2025-07-26 11:19:39 +0800

    workaround socket2 only_v6 debug panic on windows

--- lib/g3-socket/src/sockopt/windows.rs
+++ lib/g3-socket/src/sockopt/windows.rs
@@ -4,6 +4,7 @@
  */
 
 use std::io;
+use std::mem::MaybeUninit;
 use std::os::windows::io::AsRawSocket;
 
 use windows_sys::Win32::Networking::WinSock;
@@ -19,6 +20,32 @@ where
             return Err(io::Error::last_os_error());
         }
         Ok(())
+    }
+}
+
+unsafe fn getsockopt<T>(socket: WinSock::SOCKET, level: i32, name: i32) -> io::Result<T>
+where
+    T: Copy,
+{
+    let mut value: MaybeUninit<T> = MaybeUninit::zeroed();
+    unsafe {
+        let mut len = size_of::<T>() as i32;
+        let ret = WinSock::getsockopt(socket, level, name, value.as_mut_ptr().cast(), &mut len);
+        if ret == WinSock::SOCKET_ERROR {
+            return Err(io::Error::last_os_error());
+        }
+        Ok(value.assume_init())
+    }
+}
+
+pub(crate) fn ipv6_only<T: AsRawSocket>(socket: &T) -> io::Result<bool> {
+    unsafe {
+        let value = getsockopt::<u32>(
+            socket.as_raw_socket() as _,
+            WinSock::IPPROTO_IPV6,
+            WinSock::IPV6_V6ONLY,
+        )?;
+        Ok(value != 0)
     }
 }
 
