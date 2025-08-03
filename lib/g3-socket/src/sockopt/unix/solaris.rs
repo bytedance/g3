092000f10bbdbcbf428c150ff0b0426ff4ad6fa4
Zhang Jingqiang <zh_jq@outlook.com>
2025-08-03 19:41:43 +0800

    g3-socket: fix TCP_CONGESTION definition on solaris

--- lib/g3-socket/src/sockopt/unix/solaris.rs
+++ lib/g3-socket/src/sockopt/unix/solaris.rs
@@ -8,13 +8,15 @@ use std::os::fd::AsRawFd;
 
 use libc::socklen_t;
 
+const TCP_CONGESTION: libc::c_int = 0x23;
+
 #[cfg(target_os = "solaris")]
 pub(crate) fn set_tcp_congestion<T: AsRawFd>(fd: &T, tcp_ca_name: &[u8]) -> io::Result<()> {
     unsafe {
         let ret = libc::setsockopt(
             fd.as_raw_fd(),
             libc::IPPROTO_TCP,
-            0x25,
+            TCP_CONGESTION,
             tcp_ca_name.as_ptr().cast(),
             tcp_ca_name.len() as socklen_t,
         );
