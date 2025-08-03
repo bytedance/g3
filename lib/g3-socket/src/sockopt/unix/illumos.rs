dc0a498dc1af864f101b0867c7ec6230eab0ee61
Zhang Jingqiang <zh_jq@outlook.com>
2025-08-03 19:41:43 +0800

    g3-socket: fix TCP_CONGESTION definition on solaris

--- lib/g3-socket/src/sockopt/unix/illumos.rs
+++ lib/g3-socket/src/sockopt/unix/illumos.rs
@@ -8,9 +8,16 @@ use std::os::fd::AsRawFd;
 
 use libc::c_int;
 
+const TCP_QUICKACK: c_int = 0x26;
+
 pub(crate) fn set_tcp_quick_ack<T: AsRawFd>(fd: &T, enable: bool) -> io::Result<()> {
     unsafe {
-        super::setsockopt(fd.as_raw_fd(), libc::IPPROTO_TCP, 0x26, enable as c_int)?;
+        super::setsockopt(
+            fd.as_raw_fd(),
+            libc::IPPROTO_TCP,
+            TCP_QUICKACK,
+            enable as c_int,
+        )?;
         Ok(())
     }
 }
