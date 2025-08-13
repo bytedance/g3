9341010650efb019b2cd2e5402d9ceb446c1bde7
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2025-08-13 19:07:55 +0800

    allow to build package for formal version from git

--- scripts/build_rpm_from_git.sh
+++ scripts/build_rpm_from_git.sh
@@ -32,25 +32,38 @@ cp "${PACKAGE}/${PACKAGE}.spec" "${SPEC_FILE}"
 SRC_VERSION=$(cargo read-manifest --offline --manifest-path "${PACKAGE}"/Cargo.toml | jq -r '.version')
 VERSION=$(rpmspec -q --srpm --qf "%{version}" "${SPEC_FILE}")
 
+set_dev_version()
+{
+	GIT_VER=$(git log -1 --pretty=format:%cdgit%h --date=format:%Y%m%d)
+	echo "Git version: ${GIT_VER}"
+
+	echo "Finalize ${SPEC_FILE}"
+	# see https://docs.fedoraproject.org/en-US/packaging-guidelines/Versioning/
+	NEW_VERSION="${VERSION}${VERSION_SYMBOL}${GIT_VER}"
+	rpmdev-bumpspec -n "${NEW_VERSION}" -c "new git snapshot build" -u "G3proxy Maintainers <g3proxy-maintainers@devel.machine>" "${SPEC_FILE}"
+}
+
 echo "Looking for previous release tag"
-TAG_REF_NAME=$(git describe --match "${PACKAGE}-v${SRC_VERSION}" || :)
+TAG_FORMAL="${PACKAGE}-v${SRC_VERSION}"
+TAG_REF_NAME=$(git describe --match "${TAG_FORMAL}" || :)
 VERSION_SYMBOL=""
 if [ -n "${TAG_REF_NAME}" ]
 then
-	VERSION_SYMBOL="^"
-	echo "This is an update for formal version ${VERSION}"
+	if [ "${TAG_REF_NAME}" != "${TAG_FORMAL}" ]
+	then
+		VERSION_SYMBOL="^"
+		echo "This is an update for formal version ${VERSION}"
+
+		set_dev_version
+	else
+		echo "Build for formal version ${TAG_FORMAL}"
+	fi
 else
 	VERSION_SYMBOL="~"
 	echo "This is an pre-release for version ${VERSION}"
+
+	set_dev_version
 fi
-
-GIT_VER=$(git log -1 --pretty=format:%cdgit%h --date=format:%Y%m%d)
-echo "Git version: ${GIT_VER}"
-
-echo "Finalize ${SPEC_FILE}"
-# see https://docs.fedoraproject.org/en-US/packaging-guidelines/Versioning/
-NEW_VERSION="${VERSION}${VERSION_SYMBOL}${GIT_VER}"
-rpmdev-bumpspec -n "${NEW_VERSION}" -c "new git snapshot build" -u "G3proxy Maintainers <g3proxy-maintainers@devel.machine>" "${SPEC_FILE}"
 
 export RUSTFLAGS="--remap-path-prefix ${HOME}=~"
 
