600127b68f2bfd4b0f51cddc0ce8349ec2dc54b2
onin <nino.skopac@gmail.com>
2025-09-08 19:21:35 +0200

    restored mod.rs

--- g3proxy/src/escape/divert_tcp/tcp_connect/mod.rs
+++ g3proxy/src/escape/divert_tcp/tcp_connect/mod.rs
@@ -13,7 +13,7 @@ use tokio::time::Instant;
 use g3_daemon::stat::remote::{ArcTcpConnectionTaskRemoteStats, TcpConnectionTaskRemoteStats};
 use g3_io_ext::{LimitedReader, LimitedWriter};
 use g3_socket::BindAddr;
-use g3_types::net::ConnectError;
+use g3_types::net::{ConnectError, Host};
 
 use super::DivertTcpEscaper;
 use crate::log::escape::tcp_connect::EscapeLogForTcpConnect;
@@ -300,25 +300,27 @@ impl DivertTcpEscaper {
         }
     }
 
-    async fn connect_via_peer(
+    pub(super) async fn tcp_connect_to(
         &self,
-        peer_proxy: &g3_types::net::UpstreamAddr,
         task_conf: &TcpConnectTaskConf<'_>,
         tcp_notes: &mut TcpConnectTaskNotes,
         task_notes: &ServerTaskNotes,
     ) -> Result<TcpStream, TcpConnectError> {
+        let peer_proxy = self.get_next_proxy(task_notes, task_conf.upstream.host());
+
         match peer_proxy.host() {
-            g3_types::net::Host::Ip(ip) => {
+            Host::Ip(ip) => {
                 self.fixed_try_connect(
-                    std::net::SocketAddr::new(*ip, peer_proxy.port()),
+                    SocketAddr::new(*ip, peer_proxy.port()),
                     task_conf,
                     tcp_notes,
                     task_notes,
                 )
                 .await
             }
-            g3_types::net::Host::Domain(domain) => {
+            Host::Domain(domain) => {
                 let resolver_job = self.resolve_happy(domain.clone())?;
+
                 self.happy_try_connect(
                     resolver_job,
                     peer_proxy.port(),
@@ -329,19 +331,6 @@ impl DivertTcpEscaper {
                 .await
             }
         }
-    }
-
-    pub(super) async fn tcp_connect_to(
-        &self,
-        task_conf: &TcpConnectTaskConf<'_>,
-        tcp_notes: &mut TcpConnectTaskNotes,
-        task_notes: &ServerTaskNotes,
-    ) -> Result<TcpStream, TcpConnectError> {
-        let peer_proxy = self.get_next_proxy(task_notes, task_conf.upstream.host());
-
-        self
-            .connect_via_peer(peer_proxy, task_conf, tcp_notes, task_notes)
-            .await
     }
 
     pub(super) async fn tcp_new_connection(
