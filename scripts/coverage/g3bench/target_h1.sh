fc22b229ecac3d53dd1257b09f8039151503b5be
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2025-10-16 12:53:27 +0800

    g3bench: add coverage testcase for http POST

--- scripts/coverage/g3bench/target_h1.sh
+++ scripts/coverage/g3bench/target_h1.sh
@@ -1,7 +1,7 @@
 
 # Http
 
-test_http()
+test_http_get()
 {
 	URL=$1
 
@@ -17,12 +17,24 @@ test_http()
 	g3bench h1 "${URL}" -x socks5h://t1:toor@g3proxy.local:1080 --ok-status 200
 }
 
-test_http http://httpbin.local/get
-test_http http://httpbin.local:2080/get
+test_http_post()
+{
+	URL=$1
+
+	g3bench h1 "${URL}" --method POST --payload 31323334 --ok-status 200
+	g3bench h1 "${URL}" --method POST --payload 31323334 --binary --ok-status 200
+	g3bench h1 "${URL}" --method POST --payload name=foo -H "Content-Type: application/x-www-form-urlencoded" --ok-status 200
+}
+
+test_http_get http://httpbin.local/get
+test_http_get http://httpbin.local:2080/get
+
+test_http_post http://httpbin.local/post
+test_http_post http://httpbin.local:2080/post
 
 # Https
 
-test_https()
+test_https_get()
 {
 	URL=$1
 
@@ -38,5 +50,17 @@ test_https()
 	g3bench h1 "${URL}" -x socks5h://t1:toor@g3proxy.local:1080 --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
 }
 
-test_https https://httpbin.local:9443/get
-test_https https://httpbin.local:2443/get
+test_https_post()
+{
+	URL=$1
+
+	g3bench h1 "${URL}" --method POST --payload 31323334 --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+	g3bench h1 "${URL}" --method POST --payload 31323334 --binary --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+	g3bench h1 "${URL}" --method POST --payload name=foo -H "Content-Type: application/x-www-form-urlencoded" --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+}
+
+test_https_get https://httpbin.local:9443/get
+test_https_get https://httpbin.local:2443/get
+
+test_https_post https://httpbin.local:9443/post
+test_https_post https://httpbin.local:2443/post
