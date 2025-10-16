768ee039f1585e57b38af1f8add7dea5a47faced
Zhang Jingqiang <zhangjingqiang@bytedance.com>
2025-10-16 12:53:27 +0800

    g3bench: add coverage testcase for http POST

--- scripts/coverage/g3bench/target_h2.sh
+++ scripts/coverage/g3bench/target_h2.sh
@@ -1,10 +1,22 @@
 
-g3bench h2 https://httpbin.local:2443/get --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+# GET
 
-g3bench h2 https://httpbin.local:2443/get -H "Accept: application/json" --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+URL=https://httpbin.local:2443/get
 
-g3bench h2 https://httpbin.local:2443/get -x http://t1:toor@g3proxy.local:8080 --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+g3bench h2 "${URL}" --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
 
-g3bench h2 https://httpbin.local:2443/get -x https://t1:toor@g3proxy.local:8443 --proxy-tls-ca-cert "${TEST_CA_CERT_FILE}" --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+g3bench h2 "${URL}" -H "Accept: application/json" --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
 
-g3bench h2 https://httpbin.local:2443/get -x socks5h://t1:toor@g3proxy.local:1080 --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+g3bench h2 "${URL}" -x http://t1:toor@g3proxy.local:8080 --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+
+g3bench h2 "${URL}" -x https://t1:toor@g3proxy.local:8443 --proxy-tls-ca-cert "${TEST_CA_CERT_FILE}" --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+
+g3bench h2 "${URL}" -x socks5h://t1:toor@g3proxy.local:1080 --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+
+# POST
+
+URL=https://httpbin.local:2443/post
+
+g3bench h2 "${URL}" --method POST --payload 31323334 --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+g3bench h2 "${URL}" --method POST --payload 31323334 --binary --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
+g3bench h2 "${URL}" --method POST --payload name=foo -H "Content-Type: application/x-www-form-urlencoded" --ok-status 200 --tls-ca-cert "${TEST_CA_CERT_FILE}"
