36c2cd04cf45e2cc0a275b086ecd547597504461
Zhang Jingqiang <zh_jq@outlook.com>
2025-12-28 20:05:36 +0800

    replace daemonize with libc::daemon

--- lib/g3-daemon/src/daemonize.rs
+++ lib/g3-daemon/src/daemonize.rs
@@ -4,9 +4,9 @@
  */
 
 use std::fs::OpenOptions;
+use std::io::Write;
 
 use anyhow::anyhow;
-use daemonize::Daemonize;
 use log::debug;
 
 use crate::opts::DaemonArgs;
@@ -20,16 +20,31 @@ pub fn check_enter(args: &DaemonArgs) -> anyhow::Result<()> {
 }
 
 fn enter(args: &DaemonArgs) -> anyhow::Result<()> {
-    let stdout = OpenOptions::new().write(true).open("/dev/null")?;
-    let stderr = OpenOptions::new().write(true).open("/dev/null")?;
-    let mut daemonize = Daemonize::new()
-        .working_directory("/tmp")
-        .stdout(stdout)
-        .stderr(stderr);
-
-    if let Some(pid_file) = &args.pid_file {
-        daemonize = daemonize.pid_file(pid_file).chown_pid_file(true);
+    #[allow(deprecated)] // daemon() deprecated on MacOS but still usable
+    unsafe {
+        let r = libc::daemon(0, 0);
+        if r != 0 {
+            let e = std::io::Error::last_os_error();
+            return Err(anyhow!("daemon(0, 0) failed: {e}"));
+        }
     }
 
-    daemonize.start().map_err(|e| anyhow!("daemonize: {e}"))
+    if let Some(pid_file) = &args.pid_file {
+        let mut pidfile = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(false)
+            .open(pid_file)
+            .map_err(|e| anyhow!("failed to open pid file {}: {e}", pid_file.display()))?;
+        pidfile
+            .lock()
+            .map_err(|e| anyhow!("failed to lock pid file {}: {e}", pid_file.display()))?;
+        pidfile
+            .set_len(0)
+            .map_err(|e| anyhow!("failed to truncate pid file {}: {e}", pid_file.display()))?;
+        let pid = rustix::process::getpid();
+        pidfile.write_fmt(format_args!("{pid}\n"))?;
+    }
+
+    Ok(())
 }
