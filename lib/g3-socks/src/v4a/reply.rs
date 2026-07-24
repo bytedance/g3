75f9f213378955ffd2506df39c196f58a48ecc78
Zhang Jingqiang <zh_jq@outlook.com>
2026-07-24 21:59:15 +0800

    g3-bench: fix support of socks4 proxy

--- lib/g3-socks/src/v4a/reply.rs
+++ lib/g3-socks/src/v4a/reply.rs
@@ -71,7 +71,7 @@ impl SocksV4Reply {
 
         let ip_bytes: [u8; 4] = buf[4..8].try_into().unwrap();
 
-        let port = ((buf[3] as u16) << 8) + (buf[4] as u16);
+        let port = ((buf[2] as u16) << 8) + (buf[3] as u16);
         let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::from(ip_bytes)), port);
 
         Ok(SocksV4Reply::new(code, addr))
